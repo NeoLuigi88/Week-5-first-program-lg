1f159d0a8d23ca404990f268f5c335a261e6cad8
NeoLuigi88 <NeoLuigi88@gmail.com>
2026-02-18 11:50:05 -0600

    Class assignment 2-18-26
    Co-authored-by: Marco2-7 <Marco2-7@users.noreply.github.com>

---
+++ in_class.py
@@ -0,0 +1,21 @@
+name = "Luis"
+
+num = 36
+
+print("Hello there, im printing text on the screen")
+
+print(name)
+
+print("my name is ",name)
+
+name = "LG"
+print("my nickname is ",name)
+
+print("What is your favorite animal? ")
+animal = input()
+print("I didnt know that ", animal, "was your favorite animal!")
+
+if num > 10:
+    print("congrats your number is greater than 10")
+else:
+    print("Sorry your number is not greater than 10")
